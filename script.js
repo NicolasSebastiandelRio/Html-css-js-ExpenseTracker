36a79947957cc1bccfb7382a88750f5c6d0207a0
Nicolás <nicolassebastiandelrio@gmail.com>
2024-10-24 16:52:46 -0300

    finished with js file

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-const transactions = [
-    {
-        id: 1,
-        name: "salary",
-        amount: 5000,
-        date: new Date(),
-        type: "income"
-    },
-    {
-        id: 2,
-        name: "haircut",
-        amount: 20,
-        date: new Date(),
-        type: "expense"
-    },
-    {
-        id: 3,
-        name: "basketball ticket",
-        amount: 150,
-        date: new Date(),
-        type: "expense"
-    },
-
-];
+const transactions = JSON.parse(localStorage.getItem
+    ('transactions'))||[];
 
 const formatter = new Intl.NumberFormat('en-US', {
     style: "currency",
@@ -32,18 +10,39 @@ const formatter = new Intl.NumberFormat('en-US', {
 const list = document.getElementById("transactionList");
 const form = document.getElementById("transactionForm");
 const status = document.getElementById("status");
+const balance = document.getElementById('balance');
+const income = document.getElementById('income');
+const expense = document.getElementById('expense');
 
-form.addEventListener("submit",addTransaction);
+form.addEventListener("submit", addTransaction);
+
+function updateTotal() {
+    const incomeTotal = transactions
+        .filter((trx) => trx.type === "income")
+        .reduce((total, trx) => total + trx.amount, 0);
+
+    const expenseTotal = transactions
+        .filter((trx) => trx.type === "expense")
+        .reduce((total, trx) => total + trx.amount, 0);
+
+    const balanceTotal = incomeTotal - expenseTotal;
+
+    balance.textContent = formatter.format(balanceTotal);
+    income.textContent = formatter.format(incomeTotal);
+    expense.textContent = formatter.format(expenseTotal * -1);
+}
 
 function renderList() {
     list.innerHTML = "";
+    status.textContent = "";
 
     if (transactions.length === 0) {
         status.textContent = "No transactions.";
         return;
-    }
+    } 
 
     transactions.forEach(({ id, name, amount, date, type }) => {
+        const sign = 'income' === type ? 1 : -1;
         const li = document.createElement("li");
         li.innerHTML = `
             <div class="name">
@@ -51,7 +50,7 @@ function renderList() {
                 <p>${new Date(date).toLocaleDateString()}</p>
             </div>
             <div class="amount ${type}">
-                <span>${formatter.format(amount)}</span>
+                <span>${formatter.format(amount * sign)}</span>
             </div>
 
             <div class="action">
@@ -68,26 +67,39 @@ function renderList() {
 }
 
 renderList();
+updateTotal();
 
-function deleteTransaction(id){
+function deleteTransaction(id) {
     const index = transactions.findIndex((trx) => trx.id === id);
-    transactions.splice(index,1);
+    transactions.splice(index, 1);
 
+    updateTotal();
+    saveTransactions();
     renderList();
 }
 
-function addTransaction(e){
+function addTransaction(e) {
     e.preventDefault();
 
     const formData = new FormData(this);
 
     transactions.push({
-        id:transactions.length+1,
-        name:formData.get("name"),
-        amount:parseFloat(formData.get("amount")),
-        date:new Date(formData.get("date")),
+        id: transactions.length + 1,
+        name: formData.get("name"),
+        amount: parseFloat(formData.get("amount")),
+        date: new Date(formData.get("date")),
+        type: 'on' === formData.get('type') ? "income" : "expense",
+
     });
 
     this.reset();
+
+    updateTotal();
+    saveTransactions();
     renderList();
+}
+
+function saveTransactions() {
+    transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+    localStorage.setItem("transactions", JSON.stringify(transactions));
 }
